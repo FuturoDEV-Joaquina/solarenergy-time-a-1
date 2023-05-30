66c3988f7c079d9adf085669593ed5ed07c676c1
Fernando-simon <120142360+Fernando-simon@users.noreply.github.com>
2023-05-30 23:05:43 +0000

    adiciona teste de minutos

--- src/Tempo.spec.js
+++ src/Tempo.spec.js
@@ -3,18 +3,33 @@
 // Dica: escreva testes para os valores 30, 350 e 3725
 
 function numeroTempo(valor) {
-    // const horas = valor * 60;
-    // const minutos = valor / 60;
-    // const segundos = valor % 60;
-// if
-    return `0h 0m ${valor}s`
+  let horas = 0 
+  let minutos = 0
+  let segundos = 0
+  
+  if (valor < 60 ) {
+segundos = valor 
+  }
+  
+  if (valor >= 60 ){
+    minutos = Math.trunc( valor / 60 ) 
+    segundos = valor % 60
+  }
+
+    return `${horas}h ${minutos}m ${segundos}s`
+
 }
 
-test("converter o numero 30 em horas 0h 0m 30s", () => {
+test("converter o numero 30 em 0h 0m 30s", () => {
     const resultado = numeroTempo(30)
         expect(resultado).toBe('0h 0m 30s');
 });
 
-test.todo("converter o numero 350 em minutos 0h 5m 50s");
+test("converter o numero 350 em minutos 0h 5m 50s", () => {
+    const resultado = numeroTempo(350)
+    expect(resultado).toBe('0h 5m 50s'); 
+});
+   
+
 
 test.todo("converter o numero em segundos 1h 2m 5s");
