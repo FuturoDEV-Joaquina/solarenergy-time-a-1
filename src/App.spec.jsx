1ca021283836bf2393513f6096b9bea688805062
Bruno Emsters <116387450+Bruno-Emsters@users.noreply.github.com>
2023-05-30 21:19:13 -0300

    remove comentários do App.spec.jsx

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,55 +1,55 @@
-// // describe("Jest", () => {
-// //   it("testing jest", () => {
-// //     expect(1 + 1).toBe(2);
-// //   });
-// // });
+// describe("Jest", () => {
+//   it("testing jest", () => {
+//     expect(1 + 1).toBe(2);
+//   });
+// });
 
 
 
-// function stringInfo(texto) {
-//     const caracteres = texto.length;
-//     const primeiroCaracter = texto[0];
-//     const ultimoCaracter = texto[texto.length -1]
+function stringInfo(texto) {
+    const caracteres = texto.length;
+    const primeiroCaracter = texto[0];
+    const ultimoCaracter = texto[texto.length -1]
 
-//     const info = {
-//         caracteres,
-//         primeiroCaracter,
-//         ultimoCaracter,
-//     }
-//     return info
-//  }
+    const info = {
+        caracteres,
+        primeiroCaracter,
+        ultimoCaracter,
+    }
+    return info
+ }
 
-// describe("String Info", () => { 
-//     test("deveria retornar um objeto com as informações de quantos cara a string possui", () => {
-//         const resultado = stringInfo("abc")
-//         expect(resultado).toEqual(
-//             expect.objectContaining({
-//                 caracteres: 3
-//             }))
-//     })
+describe("String Info", () => { 
+    test("deveria retornar um objeto com as informações de quantos cara a string possui", () => {
+        const resultado = stringInfo("abc")
+        expect(resultado).toEqual(
+            expect.objectContaining({
+                caracteres: 3
+            }))
+    })
 
-//     test("deveria retornar um objeto com as informações de qual seu primeiro caracter", () => {
-//         const resultado = stringInfo("abc")
-//         expect(resultado).toEqual(
-//             expect.objectContaining({
-//                 primeiroCaracter: "a",
-//             }))
-//     })
+    test("deveria retornar um objeto com as informações de qual seu primeiro caracter", () => {
+        const resultado = stringInfo("abc")
+        expect(resultado).toEqual(
+            expect.objectContaining({
+                primeiroCaracter: "a",
+            }))
+    })
 
-//     test("deveria retornar um objeto com a informação do último caracter", () => {
-//         const resultado = stringInfo("abc")
-//         expect(resultado).toEqual(
-//             expect.objectContaining({
-//                 ultimoCaracter: "c",
-//             }))
-//     })
+    test("deveria retornar um objeto com a informação do último caracter", () => {
+        const resultado = stringInfo("abc")
+        expect(resultado).toEqual(
+            expect.objectContaining({
+                ultimoCaracter: "c",
+            }))
+    })
 
-//     test("deveria retornar um objeto com todas as informação", () => {
-//         const resultado = stringInfo("abc")
-//         expect(resultado).toEqual({
-//             caracteres: 3,
-//             primeiroCaracter: "a",
-//             ultimoCaracter: "c"
-//         });
-//     })
-// })
+    test("deveria retornar um objeto com todas as informação", () => {
+        const resultado = stringInfo("abc")
+        expect(resultado).toEqual({
+            caracteres: 3,
+            primeiroCaracter: "a",
+            ultimoCaracter: "c"
+        });
+    })
+})
