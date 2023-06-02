67a9935938a476968a4edfed79793998cefbc751
Brunaflores <114126289+Brunaflores@users.noreply.github.com>
2023-06-01 21:13:32 -0300

    adiciona testes para o botão dashboard do navbar

--- src/components/molecules/Navbar.spec.jsx
+++ src/components/molecules/Navbar.spec.jsx
@@ -1,15 +1,56 @@
-import { BrowserRouter } from "react-router-dom"
+import { Await, BrowserRouter } from "react-router-dom"
 import { render, screen } from "@testing-library/react";
 import Navbar from "./Navbar";
+import userEvent from '@testing-library/user-event'
+
+const mockNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useNavigate: () => mockNavigate,
+}));
+
+
+const user = userEvent.setup()
+
+function renderComponent() {
+    render(
+        <BrowserRouter>
+            <Navbar />
+        </BrowserRouter>)
+}
 
 describe("Navbar", () => {
-    it("deveria ter um titulo DevInHouse - Navbar", () => {
-        render(
-            <BrowserRouter>
-                <Navbar />
-            </BrowserRouter>)
 
+    it("deveria ter um botão para a página Dashboard",() => {
+        renderComponent()
+        const buttonDashboard = screen.getByRole("button", { name: /Dashboard/i });
+        expect(buttonDashboard).toBeInTheDocument();
+        
+    })
+
+
+    it("deveria ter um titulo DevInHouse - Navbar", () => {
+        renderComponent()
         const titulo = screen.getByText("DevInHouse - Navbar");
         expect(titulo).toBeInTheDocument();
+
+
+
     })
+
+    it("deveria redirecionar para a página dashboard quando clica no botão dashboard",async() => {
+        renderComponent()
+        const buttonDashboard = screen.getByRole("button", { name: /Dashboard/i });
+
+        await user.click(buttonDashboard)
+
+        expect(mockNavigate).toHaveBeenCalledWith("/")
+        
+
+    })
+
+
+
+
+        
 })
